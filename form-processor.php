fd106df6063b1c3add0cec2f8180faed19dbd1e0
Mike Henry <mike.henry047@gmail.com>
2018-05-25 20:55:49 -0400

    MVP~

---
+++ form-processor.php
@@ -0,0 +1,28 @@
+<?php
+
+$name = trim(stripslashes($_POST['Name']));
+$email = trim(stripslashes($_POST['Email']));
+$phone = trim(stripslashes($_POST['Phone']));
+$address = trim(stripslashes($_POST['Address']));
+
+$emailFrom = $_POST[$email];
+$emailTo = $_POST['mike.henry047@gmail.com'];
+$subject = $POST['Rec Rev Pickup'];
+
+//TODO checkbox
+
+$body .= "Name: ";
+$body .= $body;
+$body .= "\n";
+
+$body .= "Phone Number: ";
+$body .= $phone;
+$body .= "\n";
+
+$body .= "Pickup Address: ";
+$body .= $address;
+$body .= "\n";
+
+mail($emailTo, $subject, $body, "From: <$emailFrom>");
+
+?>
